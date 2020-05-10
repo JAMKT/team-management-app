f37fdc1f82d8d812474eb6b5a12165c9c1e769ca
jose <josecarlosesparza@gmail.com>
2020-05-10 20:55:36 +0200

    gitignore cache testing

--- config/dbKeys.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV === 'production') {
-    module.exports = { mongoURI: '' }; // Add your remote db connection string here
-} else {
-    module.exports = { mongoURI: '' }; // Add your local db connection string here
-}
