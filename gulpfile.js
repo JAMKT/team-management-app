d0e9c3f6c76b2a8a468964828d180952db9e2f94
Alessia Trianti <alestrianti@gmail.com>
2020-05-18 17:47:54 +0200

    Add Gulp (compile SASS and minify CSS)

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+
+const paths = {
+    src: './client/src/styles',
+    build: './client/public/styles'
+};
+
+// Compiles SASS to CSS
+gulp.task("compileSass", () => { 
+    return gulp.src(paths.src + '/*.scss')
+        .pipe(concat('index.css'))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.build + '/css')); 
+});
+
+// Minifies CSS files
+gulp.task("minifyCss", () => {
+    return gulp.src(paths.build + '/css/*.css')
+        .pipe(cssnano())
+        .pipe(concat('index.min.css'))
+        .pipe(gulp.dest(paths.build + '/minicss'));
+});
+
+// Checks for changes and runs the tasks above
+gulp.task("watch", () => { 
+    gulp.watch(paths.src + '/*.scss', gulp.series('compileSass'));
+    gulp.watch(paths.build + '/css/*.css', gulp.series('minifyCss'));
+});
+
+gulp.task("default", gulp.series('watch'));
