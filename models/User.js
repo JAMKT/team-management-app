2e1b00c40da6b6e5cd25fef8f3f74c77bbcad6b3
jose <josecarlosesparza@gmail.com>
2020-05-20 09:43:03 +0200

    Minor changes user model

--- models/User.js
+++ models/User.js
@@ -21,6 +21,7 @@ const userSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "Job"
     },
+    description: String,
     contacts: [
         {
             user: {
