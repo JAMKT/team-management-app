22136b8bde4869debb6b3179adbf78f04a7aa681
jose <josecarlosesparza@gmail.com>
2020-05-31 12:14:05 +0200

    Data on Activity Feed

--- client/src/components/Common/LiveFeed/FeedItem.js
+++ client/src/components/Common/LiveFeed/FeedItem.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export default function FeedItem() {
+export default function FeedItem(props) {
     return (
         <div className="row feed-item">
             <div className="col comment-icon-col">
                 <img className="user-icon" src="https://via.placeholder.com/32" />
             </div>
             <div className="col">
-                <h4>Commented on Chat App</h4>
-                <span>12 minutes ago</span>
-                <p>Activity feed text goes here...</p>
+                <h4>{props.feed.title}</h4>
+                <span>{props.feed.time} minutes ago</span>
+                <p>{props.feed.content}</p>
             </div>
         </div>
     )
