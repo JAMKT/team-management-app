1e5ce753de269332d5d1eecb8a1ead6976dbd0a5
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2020-06-05 11:03:20 +0200

    Write simple tests in ProfileInfo.test.js
    Also generate a snapshot. Notice! Tests and snapshots should not go to production!

---
+++ client/src/components/Profile/ProfileInfo.test.js
@@ -0,0 +1,15 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import ProfileInfo from './ProfileInfo';
+
+//Testing if the ProfileInfo component is rendered
+it('expect to render ProfileInfo component', () => {
+    expect(shallow(<ProfileInfo />).length).toEqual(1)
+})
+
+//Take a snapshot of a component to track changes
+//The test will fail if the changes are made in ProfileInfo component
+//If you intended those changes, press u if the test fails to update the snapshot
+it('expect to render ProfileInfo component (make a snapshot)', () => {
+    expect(shallow(<ProfileInfo />).debug()).toMatchSnapshot();
+})
